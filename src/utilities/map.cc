a94bbd54a1169c50fd4d053c2e8a401c10dd58e2
Erwan Auer <erwan.auer@epita.fr>
2020-11-11 04:57:11 +0100

    map: add hashmap class

---
+++ src/utilities/map.cc
@@ -0,0 +1,42 @@
+#include "map.hh"
+
+#include <stdexcept>
+
+namespace utilities {
+
+template <typename Key, typename T, typename Hash, typename Eq>
+map<Key, T, Hash, Eq>::map()
+    : bucket_count_(DEFAULT_BUCKET_COUNT),
+      buckets_(list<bucket>(bucket_count_)), hash_function_(Hash()),
+      eq_function_(Eq()) {}
+
+template <typename Key, typename T, typename Hash, typename Eq>
+bool map<Key, T, Hash, Eq>::insert(const Key &key, const T &element) {
+  int hash = hash_function_(key);
+  bucket &b = buckets_[hash % bucket_count_];
+  ssize_t index = b.keys_.find(key);
+  if (index == -1) {
+    b.keys_.insert(key);
+    b.elements_.insert(element);
+    return true;
+  }
+  return false;
+}
+
+template <typename Key, typename T, typename Hash, typename Eq>
+T &map<Key, T, Hash, Eq>::operator[](const Key &key) {
+  int hash = hash_function_(key);
+  bucket &b = buckets_[hash % bucket_count_];
+  ssize_t index = b.keys_.find(key);
+  if (index == -1)
+    throw std::invalid_argument("Key not in map");
+  return b.elements_[index];
+}
+template <typename Key, typename T, typename Hash, typename Eq>
+bool map<Key, T, Hash, Eq>::contains(const Key &key) {
+  int hash = hash_function_(key);
+  bucket &b = buckets_[hash % bucket_count_];
+  ssize_t index = b.keys_.find(key);
+  return index != -1;
+}
+} // namespace utilities
---
+++ src/utilities/map.hh
@@ -0,0 +1,27 @@
+#pragma once
+
+#define DEFAULT_BUCKET_COUNT 100
+
+#include "list.hh"
+
+namespace utilities {
+template <typename Key, typename T, typename Hash, typename Eq> class map {
+public:
+  map();
+
+  bool insert(const Key &key, const T &element);
+
+  T &operator[](const Key &key);
+  bool contains(const Key &key);
+
+private:
+  struct bucket {
+    list<Key> keys_;
+    list<T> elements_;
+  };
+  size_t bucket_count_;
+  list<bucket> buckets_;
+  Hash hash_function_;
+  Eq eq_function_;
+};
+} // namespace utilities
